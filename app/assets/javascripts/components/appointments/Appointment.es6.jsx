a163b9b691593e5a2975b1f294180f0ac7d2715c
sjBao <supuilam@gmail.com>
2017-02-21 02:50:38 -0500

    Add style to calendar app

--- app/assets/javascripts/components/appointments/Appointment.es6.jsx
+++ app/assets/javascripts/components/appointments/Appointment.es6.jsx
@@ -2,7 +2,7 @@ class Appointment extends React.Component {
   render(){
     let { id, title, apt_time } = this.props.appointment
     return(
-      <div>
+      <div className='appointment'>
         <h3>{title}</h3>
         <p>{moment(apt_time).format('MMMM DD YYYY, h:mm a')}</p>
       </div>
